3df751bfd6098345f0461230d58ea896129fef79
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2024-09-09 20:12:32 +0700

    formik - yup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,38 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useState } from "react";
 import { Button, Col, Container, FormGroup, FormLabel, Row } from "react-bootstrap";
-
+import * as Yup from 'yup';
 const Regsiter = ()=>{
+    const [submitting,setSubmitting] = useState(false);
+    const validationSchema = Yup.object({
+        fullname: Yup.string()
+            .min(6,"Độ dài tối thiểu là 6")
+            .required("Vui lòng nhập thông tin"),
+        email: Yup.string()
+            .email("Vui lòng nhập đúng email")
+            .required("Vui lòng nhập thông tin"),
+        password: Yup.string()
+            .min(6,"Độ dài tối thiểu là 6")
+            .required("Vui lòng nhập thông tin")
+    });
+    const values = {
+        fullname:'',
+        email:'',
+        password:''
+    }
+    const formsubmit = ()=>{
+        setSubmitting(true);
+    }
     return (
         <Container>
             <Row>
                 <Col></Col>
                 <Col>
                     <h1>Register</h1>
-                    <Formik>
+                    <Formik initialValues={values}
+                        validationSchema={validationSchema}
+                        onSubmit={formsubmit}
+                    >
                         <Form>
                             <FormGroup className="mb-3 text-start">
                                 <FormLabel>Fullname</FormLabel>
@@ -26,7 +50,7 @@ const Regsiter = ()=>{
                                 <ErrorMessage name="password" component='p' style={{color: 'red'}} />
                             </FormGroup>
                             <FormGroup className="mb-3 text-start">
-                                <Button variant="primary" type="submit">Register</Button>
+                                <Button variant="primary" type="submit" disabled={submitting}>Register</Button>
                             </FormGroup>
                         </Form>
                     </Formik>
