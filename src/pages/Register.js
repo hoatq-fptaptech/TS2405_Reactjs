4e807dd5c21fb6191dde64e720c5d28010b09c3e
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2024-09-09 19:49:41 +0700

    form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Button, Col, Container, FormGroup, FormLabel, Row } from "react-bootstrap";
 
 const Regsiter = ()=>{
     return (
@@ -6,7 +7,29 @@ const Regsiter = ()=>{
             <Row>
                 <Col></Col>
                 <Col>
-                    
+                    <h1>Register</h1>
+                    <Formik>
+                        <Form>
+                            <FormGroup className="mb-3 text-start">
+                                <FormLabel>Fullname</FormLabel>
+                                <Field type="text" name="fullname" className="form-control"/>
+                                <ErrorMessage name="fullname" component='p' style={{color: 'red'}} />
+                            </FormGroup>
+                            <FormGroup className="mb-3 text-start">
+                                <FormLabel>Email</FormLabel>
+                                <Field type="email" name="email" className="form-control"/>
+                                <ErrorMessage name="email" component='p' style={{color: 'red'}} />
+                            </FormGroup>
+                            <FormGroup className="mb-3 text-start">
+                                <FormLabel>Password</FormLabel>
+                                <Field type="password" name="password" className="form-control"/>
+                                <ErrorMessage name="password" component='p' style={{color: 'red'}} />
+                            </FormGroup>
+                            <FormGroup className="mb-3 text-start">
+                                <Button variant="primary" type="submit">Register</Button>
+                            </FormGroup>
+                        </Form>
+                    </Formik>
                 </Col>
                 <Col></Col>
             </Row>
