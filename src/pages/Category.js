fc1a2e112cf7dbb5dd8071bc789bc749abeed839
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2024-09-12 20:18:58 +0700

    demo use params

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,7 +1,24 @@
-const Category = ()=>{
+import { useEffect, useState } from "react";
+import Grid from "../components/Products/Grid";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
+const Category = ()=>{
+    const {slug} = useParams();
+    const [products,setProducts] = useState([]);
+    const getProductsData = async ()=>{
+        const rs = await fetch("https://dummyjson.com/products/category/"+slug);
+        const data = await rs.json();
+        setProducts(data.products);
+    }
+    useEffect(()=>{
+        getProductsData();
+    },[])
     return (
-        <h1>Category</h1>
+        <Container>
+            <h1 className="text-capitalize">{slug}</h1>  
+            <Grid products={products}/>
+        </Container>
     )
 }
 export default Category;
